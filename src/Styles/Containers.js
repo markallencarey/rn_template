4f0b5eb846b549e2e02b4e4bc4c8d62de2a1e1e8
Mark Carey <markallencarey@gmail.com>
2022-01-28 15:03:31 -0700

    adjusted content and appView styling

--- src/Styles/Containers.js
+++ src/Styles/Containers.js
@@ -14,22 +14,14 @@ const styles = StyleSheet.create({
 		width: Window.width,
 		height: Window.height,
 		backgroundColor: Colors.background,
+		paddingHorizontal: Misc.padding * 2,
 		...centered,
 	},
-	safeAreaView: {
-		width: '100%',
-		height: '100%',
-	},
 	content: {
-		width: '100%',
-		height: '100%',
-		padding: Misc.padding,
 		flex: 1,
-		backgroundColor: Colors.background,
 		...centered,
 	},
 })
 
 export const appView = styles.appView
-export const safeAreaView = styles.safeAreaView
 export const content = styles.content
