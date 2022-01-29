a53300d10bd3e476529cf73e2536ec90d264abad
Mark Carey <markallencarey@gmail.com>
2022-01-29 16:03:26 -0700

    fixed firebase w/ screens

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,10 +1,10 @@
 //React & React Native
 import React, { useContext, useEffect, useState } from 'react'
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native'
 //Packages
 //Context
 //Constants
-import { firestore } from '../../Constants/Firebase'
+import { firestore, timestamp } from '../../Constants/Firebase'
 //Navigation
 //Components
 //Screens
@@ -15,71 +15,79 @@ import { firestore } from '../../Constants/Firebase'
 import { Buttons, Colors, Containers, Fonts, Icons, Images, Index, Misc, Window } from '../../Styles/Index'
 
 export const Home = ({ navigation }) => {
-  const [textPosts, setTextPosts] = useState([])
-  const [textInput, setTextInput] = useState('')
+	const [textPosts, setTextPosts] = useState([])
+	const [textInput, setTextInput] = useState('')
 
-  useEffect(() => {
-    firestore()
-      .collection('Test')
-      .onSnapshot(
-        querySnapshot => {
-          const testPosts = []
-          querySnapshot?.forEach(doc => {
-            testPosts.push(doc.data())
-          })
-          setTextPosts(testPosts)
-        },
-        err => console.log(err)
-      )
-  }, [])
-  
-  const goToScreen1 = () => {
-    navigation.navigate('Screen1')
-  }
+	useEffect(() => {
+		firestore()
+			.collection('Test')
+			.orderBy('date', 'desc')
+			.onSnapshot(
+				querySnapshot => {
+					const testPosts = []
+					querySnapshot?.forEach(doc => {
+						testPosts.push(doc.data())
+					})
+					setTextPosts(testPosts)
+				},
+				err => console.log(err)
+			)
+	}, [])
 
-  const goToScreen2 = () => {
-    navigation.navigate('Screen2')
-  }
+	const testPost = () => {
+		firestore().collection('Test').add({
+			text: textInput,
+			date: timestamp,
+		})
+		setTextInput('')
+	}
 
-  return (
-    <View style={styles.content}>
-      <Text style={styles.h2}>Home.js</Text>
-      <View style={styles.navRow}>
-        <TouchableOpacity style={styles.navBtn} onPress={goToScreen1}>
-          <Text style={styles.body}>Go to</Text>
-          <Text style={styles.body}>Screen1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.navBtn} onPress={goToScreen2}>
-          <Text style={styles.body}>Go to</Text>
-          <Text style={styles.body}>Screen2</Text>
-          
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text style={styles.firebaseTest}>Firebase Test:</Text>
-        <Text style={styles.testTxt}>
-          Below you can test that your Firebase connection is working. Type something into the text input and press Post. If your Firebase and
-          Firestore are set up correctly should post to Firestore and then display in a FlatList at the bottom.
-        </Text>
-        <TextInput onChangeText={input => setTextInput(input)} style={styles.input} placeholder='enter test text here' value={textInput} />
-        <TouchableOpacity
-          onPress={() => {
-            firestore().collection('Test').add({ text: textInput })
-            setTextInput('')
-          }}
-          style={styles.postBtn}
-        >
-          <Text>Post</Text>
-        </TouchableOpacity>
-        <FlatList
-          data={textPosts}
-          renderItem={({ item }) => <Text style={styles.testTxt}>{item.text}</Text>}
-          keyExtractor={item => item.id}
-          style={styles.flatList}
-        />
-      </View>
-    </View>
-  )
+	const goToScreen1 = () => {
+		navigation.navigate('Screen1')
+	}
+
+	const goToScreen2 = () => {
+		navigation.navigate('Screen2')
+	}
+
+	return (
+		<View style={styles.content}>
+			<Text style={styles.h2}>Home.js</Text>
+			<View style={styles.navRow}>
+				<TouchableOpacity style={styles.navBtn} onPress={goToScreen1}>
+					<Text style={styles.body}>Go to</Text>
+					<Text style={styles.body}>Screen1</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.navBtn} onPress={goToScreen2}>
+					<Text style={styles.body}>Go to</Text>
+					<Text style={styles.body}>Screen2</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.firebase}>
+				<Text style={styles.h2}>Firebase Test:</Text>
+				<Text style={styles.testTxt}>
+					Below you can test that your Firebase connection is working. Type something into the text input and press Post. If your Firebase and
+					Firestore are set up correctly should post to Firestore and then display in a FlatList at the bottom.
+				</Text>
+				<TextInput
+					onChangeText={input => setTextInput(input)}
+					style={styles.input}
+					placeholder='enter test text here'
+					value={textInput}
+					onSubmitEditing={testPost}
+				/>
+				<TouchableOpacity onPress={testPost} style={styles.postBtn}>
+					<Text>Post</Text>
+				</TouchableOpacity>
+				<FlatList
+					data={textPosts}
+					renderItem={({ item }) => <Text style={styles.testTxt}>{item.text}</Text>}
+					keyExtractor={item => item.id}
+					style={styles.flatList}
+				/>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
@@ -100,6 +108,12 @@ const styles = StyleSheet.create({
 		...Buttons.transparent,
 		marginHorizontal: Misc.margin,
 		justifyContent: 'flex-start',
+	},
+	firebase: {
+		width: '100%',
+		alignItems: 'center',
+		marginTop: Misc.padding * 2,
+		height: '70%',
 	},
 	firebaseTest: {
 		...Fonts.h2,
@@ -124,7 +138,7 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		borderRadius: Misc.borderRadius,
 		width: '100%',
-		maxHeight: Window.height * 0.3,
+		height: Window.height * 0.5,
 		paddingHorizontal: Misc.padding,
 	},
 })
